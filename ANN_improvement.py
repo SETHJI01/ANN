aa56f91181eed5ca51746707efb14ce1af55bc3d
Arpit Seth <setharpit11@gmail.com>
2021-05-30 14:44:39 +0530

    parameter tuning

--- ANN_improvement.py
+++ ANN_improvement.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Created on Sun May 30 13:26:23 2021
+Created on Sun May 30 14:23:15 2021
 
 @author: sethji
 """
@@ -40,26 +40,29 @@ X_test=sc.transform(X_test)
 
 classifier=tf.keras.models.Sequential()
 classifier.add(tf.keras.layers.Dense(units=6, activation = 'relu'))
+classifier.add(tf.keras.layers.Dropout(p=0.1))
 classifier.add(tf.keras.layers.Dense(units = 6 ,activation = 'relu'))
+classifier.add(tf.keras.layers.Dropout(p=0.1))
 classifier.add(tf.keras.layers.Dense(units = 1 , activation = 'sigmoid'))
 classifier.compile(optimizer='adam',loss='binary_crossentropy',metrics=['accuracy'])
 classifier.fit(X_train,Y_train,batch_size=10,epochs=100)
 
+#evaluating the ANN
 import keras
 from keras.wrappers.scikit_learn import KerasClassifier
 from sklearn.model_selection import GridSearchCV
-
-def build_classifier():
+def build_classifier(optimizer):
     classifier=tf.keras.models.Sequential()
     classifier.add(tf.keras.layers.Dense(units=6, activation = 'relu'))
     classifier.add(tf.keras.layers.Dense(units = 6 ,activation = 'relu'))
     classifier.add(tf.keras.layers.Dense(units = 1 , activation = 'sigmoid'))
-    classifier.compile(optimizer='adam',loss='binary_crossentropy',metrics=['accuracy'])
+    classifier.compile(optimizer=optimizer,loss='binary_crossentropy',metrics=['accuracy'])
     return classifier
+#improvement Tuning
 classifier = KerasClassifier(build_fn = build_classifier)
-parameters = {'batch_size':[10],
-              'epochs': [100],
-             }
+parameters = {'batch_size':[25,32],
+              'epochs': [100,400],
+              'optimizer': ['adam','rmsprop']}
 grid_search = GridSearchCV(estimator = classifier,
                            param_grid = parameters,
                            scoring = 'accuracy',
